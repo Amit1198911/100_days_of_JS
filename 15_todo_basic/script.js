dbc672e6de1ffacd015ea0d97361d485f9922bf2
Amit1198911 <amitahirwar1198911@gmail.com>
2024-10-04 00:26:34 +0530

    CRUD

--- 15_todo_basic/script.js
+++ 15_todo_basic/script.js
@@ -1,18 +1,26 @@
 const input = document.querySelector('#todo-input');
 const todolist = document.querySelector('#todo-list');
-
 const add = document.querySelector('#add-btn');
+
 add.addEventListener('click',(e)=>{
     if(input.value===''){
         alert("Please write something ...");
+        return;
     }
+
+
     let note = document.createElement('li');
-    let list = `${input.value}`;
-    note.textContent = list; 
+    let list = document.createElement('span');
+    list.textContent = input.value;
+    note.appendChild(list); 
 
     let deletebtn = document.createElement('button');
-    deletebtn.textContent = 'X';
+    deletebtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
     note.appendChild(deletebtn);
+
+    let editbtn = document.createElement('button');
+    editbtn.innerHTML = '<i class="fa-solid fa-pen-to-square"></i>';
+    note.appendChild(editbtn);
 
     note.style.color = "black"  
     todolist.appendChild(note);
@@ -20,6 +28,13 @@ add.addEventListener('click',(e)=>{
 
     deletebtn.addEventListener('click',(e)=>{
             e.target.parentElement.remove();
+    })
+
+    editbtn.addEventListener('click',(e)=>{
+        const newTask = prompt("Enter a new task",list.textContent);
+        if (newTask !== null && newTask.trim() !== '') {
+            list.textContent = newTask; 
+        }
     })
 })
 
